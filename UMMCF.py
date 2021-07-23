d14c68531709b2e6932a8ed4a5af746e5bd6db98
Samuele Pilleri <pilleri.samuele@educ.di.unito.it>
2021-07-24 01:18:07 +0200

    Working model

--- UMMCF.py
+++ UMMCF.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas.core.algorithms import mode
 import pyomo.environ as pyo
 
 topology = pd.read_csv("dummy_topology.csv")
@@ -17,10 +16,7 @@ nodes = sorted(starts | ends)
 
 model.Nodes = pyo.Set(initialize=nodes)
 model.Arcs = pyo.Set(initialize=list(topology.index))
-model.Arcs2 = pyo.RangeSet(0, len(topology.reset_index().index) - 1)
 model.K = pyo.RangeSet(0, len(flows.index) - 1)
-
-model.c = pyo.Param(model.Arcs2, within=pyo.NonNegativeIntegers)
 
 model.X = pyo.Var(model.Arcs, model.K, within=pyo.Binary) # prendo il flusso o non lo prendo
 model.alpha = pyo.Var(within=pyo.PercentFraction) # alpha è compreso tra 0 e 1
@@ -36,34 +32,27 @@ def traffic_constraint(m, i, k):
     succs = arcs[ arcs.Start == i ]["End"]
     s, t, d = flows.loc[k]
     if i != s and i != t:
-        print(sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == 0)
         return sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == 0
     elif i == s:
-        print(sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == d)
         return sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == d
     elif i == t:
-        print(sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == -d)
         return sum(m.X[i, j, k]*d for j in succs) - sum(m.X[j, i, k]*d for j in precs) == -d
 
 model.traffic_constraint = pyo.Constraint(model.Nodes, model.K, rule=traffic_constraint)
 
-# def capacity_constraint(m, i, j):
-def capacity_constraint(m, a):
-    i, j, c = topology.reset_index().loc[a]
-    # return sum(m.X[i, j, k] for k in m.K) <= m.c[a] * m.alpha
-    print(sum(m.X[i, j, k] * flows.loc[k]["Demand"] for k in m.K) <= c * m.alpha)
+def capacity_constraint(m, i, j):
+    c = int(topology.loc[i, j]["Capacity"])
     return sum(m.X[i, j, k] * flows.loc[k]["Demand"] for k in m.K) <= c * m.alpha
-    # return m.alpha >= 0
 
-model.capacity_constraint = pyo.Constraint(model.Arcs2, rule=capacity_constraint)
+model.capacity_constraint = pyo.Constraint(model.Arcs, rule=capacity_constraint)
 
 solver = pyo.SolverFactory("cbc")
 results = solver.solve(model, tee=True, keepfiles=False)
 
 if results.solver.status != pyo.SolverStatus.ok:
-    print('Check solver not ok?')
+    print("Check solver not ok?")
 if results.solver.termination_condition != pyo.TerminationCondition.optimal:  
-    print('Check solver optimality?')
+    print("Check solver optimality?")
 
 print("Solution: ", model.OBJ())
 print(model.X.pprint())
